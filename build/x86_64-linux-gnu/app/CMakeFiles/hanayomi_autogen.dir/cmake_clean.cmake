303058ef69582aff0ac8d6243dd5714574781d60
Hakim <shanun036@gmail.com>
2026-06-26 19:43:12 +0800

    Ignore build directory

--- build/x86_64-linux-gnu/app/CMakeFiles/hanayomi_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/hanayomi_autogen"
-  "hanayomi_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/hanayomi_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
